dee7a69a1498ac51fbb28a393c05bd7c8fc35eba
Rodolfo Sparapan <rodolfosparapan@gmail.com>
2020-05-30 15:54:45 -0300

    Adicionando healthcheck e swagger

--- src/BarDG.Api/Startup.cs
+++ src/BarDG.Api/Startup.cs
@@ -4,13 +4,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace BarDG.Api
 {
     public class Startup
     {
         private readonly IConfiguration configuration;
-
+        private const string apiName = "BarDG";
         public Startup(IConfiguration configuration)
         {
             this.configuration = configuration;
@@ -20,7 +21,14 @@ namespace BarDG.Api
         {
             DependencyInjection.Register(services, configuration);
 
+            services.AddHealthChecks();
+
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = apiName, Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -36,8 +44,16 @@ namespace BarDG.Api
 
             app.UseAuthorization();
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", apiName);
+            });
+
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHealthChecks("/health");
                 endpoints.MapControllers();
             });
         }
